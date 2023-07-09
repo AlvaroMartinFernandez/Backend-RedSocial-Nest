d5c07499f499536efbac4d333af809a1d585cc68
AlvaroMartinFernandez <alvaro.martin@edu.uah.es>
2023-07-09 13:08:29 -0500

    Finalizado

--- src/user/post.service.ts
+++ src/user/post.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { Post, Data } from './post.controller';
+import fetch, { Response } from 'node-fetch';
 @Injectable()
 export class PostService {
   async findOne(id: number): Promise<{ statusCode: number; data: Data[] }> {
     try {
       const response: Response = await fetch(`${process.env.POSTAPI}${id}`);
-      const data: Post = await response.json();
+      const data: any = await response.json();
       console.log('Ejecutamos el servicio');
       return data;
     } catch (e: any) {
