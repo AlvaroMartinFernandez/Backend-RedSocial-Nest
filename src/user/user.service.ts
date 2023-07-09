a05bdeebdaac84c2450149256720de7da0cf7b8f
AlvaroMartinFernandez <alvaro.martin@edu.uah.es>
2023-07-09 16:02:18 -0500

    Finalizado

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,83 +20,110 @@ export class UserService {
     private userDataModel: typeof DataUser,
   ) {}
   async create(createUserDto: CreateUserDto) {
-    return this.userModel.create(createUserDto);
+    try {
+      return this.userModel.create(createUserDto);
+    } catch (e) {
+      console.log(e);
+      return e;
+    }
   }
 
   async findAll(): Promise<User[]> {
-    const users = await this.userModel.findAll({
-      attributes: ['id', 'name', 'photourl', 'description'],
-    });
-    if (users.length === 0) {
-      throw new NotFoundException(`Users not found`);
+    try {
+      const users = await this.userModel.findAll({
+        attributes: ['id', 'name', 'photourl', 'description'],
+      });
+      if (users.length === 0) {
+        throw new NotFoundException(`Users not found`);
+      }
+      return users;
+    } catch (e) {
+      console.log(e);
+      return e;
     }
-    return users;
   }
 
   async findOne(id: number): Promise<DataUser> {
-    const user = await this.userDataModel.findOne({
-      where: { id },
-      include: [
-        {
-          model: Friend,
-          include: [
-            {
-              model: User,
-              attributes: ['name', 'photourl'],
-              as: 'friendUser',
-            },
-          ],
-        },
-        { model: Pet },
-      ],
-      attributes: ['id', 'name', 'photourl', 'description'],
-    });
-    if (!user) {
-      throw new NotFoundException(`User with id ${id} not found`);
+    try {
+      const user = await this.userDataModel.findOne({
+        where: { id },
+        include: [
+          {
+            model: Friend,
+            include: [
+              {
+                model: User,
+                attributes: ['name', 'photourl'],
+                as: 'friendUser',
+              },
+            ],
+          },
+          { model: Pet },
+        ],
+        attributes: ['id', 'name', 'photourl', 'description'],
+      });
+      if (!user) {
+        throw new NotFoundException(`User with id ${id} not found`);
+      }
+      return user;
+    } catch (e) {
+      console.log(e);
+      return e;
     }
-    return user;
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    const user = await this.userModel.findByPk(id);
-    if (!user) {
-      throw new NotFoundException(`User with id ${id} not found`);
+    try {
+      const user = await this.userModel.findByPk(id);
+      if (!user) {
+        throw new NotFoundException(`User with id ${id} not found`);
+      }
+      console.log(user);
+      updateUserDto.updatedAt = new Date();
+      console.log(updateUserDto);
+      const data = await this.userModel.update(updateUserDto, {
+        where: { id },
+      });
+      return data;
+    } catch (e) {
+      console.log(e);
+      return e;
     }
-    console.log(user);
-    updateUserDto.updatedAt = new Date();
-    console.log(updateUserDto);
-    const data = await this.userModel.update(updateUserDto, { where: { id } });
-    return data;
   }
 
   async remove(id: number) {
-    const data = await this.userDataModel.findOne({
-      where: { id },
-      include: [{ model: Friend }, { model: Pet }],
-    });
-    if (!data) {
-      throw new NotFoundException(`User with id ${id} not found`);
-    }
-    if (data.Friends.length > 0) {
-      data.Friends.map(async (element) => {
-        await this.friendModel.destroy({
-          where: {
-            [Op.or]: [
-              { user_id: element.user_id },
-              { friend_id: element.friend_id },
-            ],
-          },
-        });
+    try {
+      const data = await this.userDataModel.findOne({
+        where: { id },
+        include: [{ model: Friend }, { model: Pet }],
       });
-    }
-    if (data.Pets.length > 0) {
-      data.Pets.map(async (element) => {
-        await this.petModel.destroy({
-          where: { user_id: element.user_id },
+      if (!data) {
+        throw new NotFoundException(`User with id ${id} not found`);
+      }
+      if (data.Friends.length > 0) {
+        data.Friends.map(async (element) => {
+          await this.friendModel.destroy({
+            where: {
+              [Op.or]: [
+                { user_id: element.user_id },
+                { friend_id: element.friend_id },
+              ],
+            },
+          });
         });
-      });
+      }
+      if (data.Pets.length > 0) {
+        data.Pets.map(async (element) => {
+          await this.petModel.destroy({
+            where: { user_id: element.user_id },
+          });
+        });
+      }
+      await this.friendModel.destroy({ where: { friend_id: id } });
+      return await this.userModel.destroy({ where: { id } });
+    } catch (e) {
+      console.log(e);
+      return e;
     }
-    await this.friendModel.destroy({ where: { friend_id: id } });
-    return await this.userModel.destroy({ where: { id } });
   }
 }
