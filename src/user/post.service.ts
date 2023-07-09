e68b22434a1e2b92aea0fa1534fbad9775adbd56
AlvaroMartinFernandez <alvaro.martin@edu.uah.es>
2023-07-09 12:26:39 -0500

    Correccion

--- src/user/post.service.ts
+++ src/user/post.service.ts
@@ -1,25 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { Post,Data } from './post.controller';
+import { Post, Data } from './post.controller';
 @Injectable()
 export class PostService {
   async findOne(id: number): Promise<{ statusCode: number; data: Data[] }> {
     try {
-      const response: Response = await fetch(`${process.env.POSTAPI}${id}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      if (!response.ok) {
-        if (response.status === 404) {
-          return { statusCode: response.status, data: [] };
-        }
-        throw new Error('Error fetching');
-      }
+      const response: Response = await fetch(`${process.env.POSTAPI}${id}`);
       const data: Post = await response.json();
       return data;
     } catch (e: any) {
-      return { statusCode: 500, data: [] };
+      return e;
     }
   }
 }
